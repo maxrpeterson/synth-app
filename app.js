dbe9b7e6dd2a3ba0a5132b2e567ff3b368f6514c
Max Peterson <petersonmaxr@gmail.com>
2015-07-17 15:45:51 -0400

    got rid of the bower components thing from gitignore

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 3000;
 
-if (app.get('env') === 'development') {
-	console.log("app is in development environment");
-	var morgan = require('morgan');
-	app.use(morgan('combined'));
-}
+// if (app.get('env') === 'development') {
+// 	console.log("app is in development environment");
+// 	var morgan = require('morgan');
+// 	app.use(morgan('combined'));
+// }
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(express.static('public'));
-app.use(express.static('bower_components'));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/bower_components'));
 
 var mongoUri = process.env.MONGOLAB_URI || "mongodb://localhost:27017/synth-app";
 
